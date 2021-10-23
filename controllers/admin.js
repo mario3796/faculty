4616c3c951e839dfd5d0f44c755ee02de87d5bcb
Mario Haleem <mariohaleem@gmail.com>
2021-10-23 22:21:06 +0200

    add postAddUser method in admin controller

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,35 @@
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+
 exports.getAddUser = (req, res, next) => {
     res.render('admin/edit-user', {
         path: '/admin/add-user',
-        title: 'Add User'
+        title: 'Add User',
+        editing: false
     });
 }
+
+exports.postAddUser = (req, res, next) => {
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
+    const email = req.body.email;
+    const password = req.body.pwd;
+    const userType = req.body.userType;
+    const department = req.body.dept;
+    
+    bcrypt.hash(password, 12)
+    .then(hashedPassword => {
+        const user = new User();
+        user.name = firstName + ' ' + lastName;
+        user.email = email;
+        user.password = hashedPassword;
+        user.user_type = userType;
+        user.department = department;
+        return user.save();
+    })
+    .then(result => {
+        console.log(result);
+        res.redirect('/');
+    })
+    .catch(err => console.log(err));
+};
