cda7cf36dec7563842f9956b2bd9c8fd0a76c31b
Mario Haleem <mariohaleem@gmail.com>
2021-10-23 22:32:45 +0200

    add a route in admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -6,4 +6,6 @@ const adminController = require("../controllers/admin");
 
 router.get('/add-user', adminController.getAddUser);
 
+router.post('/add-user', adminController.postAddUser)
+
 module.exports = router;
