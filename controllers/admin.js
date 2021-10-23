cd7014820ef31d57f725f2b6d6cafee068b357f4
Mario Haleem <mariohaleem@gmail.com>
2021-10-23 23:14:19 +0200

    edit getAddUser method

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,39 +2,35 @@ const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 
 exports.getAddUser = (req, res, next) => {
-    User.find()
-    .then(users => {
-        res.render('admin/edit-user', {
-            path: '/admin/add-user',
-            title: 'Add User',
-            editing: false,
-            users: users
-        });
-    })
-    .catch(err => console.log(err));
-}
+  res.render("admin/edit-user", {
+    path: "/admin/add-user",
+    title: "Add User",
+    editing: false,
+  });
+};
 
 exports.postAddUser = (req, res, next) => {
-    const firstName = req.body.fname;
-    const lastName = req.body.lname;
-    const email = req.body.email;
-    const password = req.body.pwd;
-    const userType = req.body.userType;
-    const department = req.body.dept;
-    
-    bcrypt.hash(password, 12)
-    .then(hashedPassword => {
-        const user = new User();
-        user.name = firstName + ' ' + lastName;
-        user.email = email;
-        user.password = hashedPassword;
-        user.user_type = userType;
-        user.department = department;
-        return user.save();
+  const firstName = req.body.fname;
+  const lastName = req.body.lname;
+  const email = req.body.email;
+  const password = req.body.pwd;
+  const userType = req.body.userType;
+  const department = req.body.dept;
+
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+      const user = new User();
+      user.name = firstName + " " + lastName;
+      user.email = email;
+      user.password = hashedPassword;
+      user.user_type = userType;
+      user.department = department;
+      return user.save();
     })
-    .then(result => {
-        console.log(result);
-        res.redirect('/');
+    .then((result) => {
+      console.log(result);
+      res.redirect("/");
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
