371f2c4374cfc117ab2b63626bc40930af491f72
Mario Haleem <mariohaleem@gmail.com>
2021-10-23 12:35:44 +0200

    Add user routes in 'app.js' file

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const mongoose = require("mongoose");
 
+const userRoutes= require("./routes/user");
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,6 +13,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(userRoutes);
 
 mongoose
 .connect("mongodb+srv://Mario:5SooxNnEpX5XvapP@cluster0.e6u5k.mongodb.net/faculty?retryWrites=true&w=majority")
