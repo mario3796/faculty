7a23c93aec5beba3600488f223bbc48e9c766ec6
Mario Haleem <mariohaleem@gmail.com>
2021-10-25 14:18:40 +0200

    add a get route to getDetails method

--- routes/user.js
+++ routes/user.js
@@ -6,4 +6,6 @@ const userController = require("../controllers/user");
 
 router.get('/', userController.getIndex);
 
+router.get('/user-details/:userId', userController.getDetails);
+
 module.exports = router;
