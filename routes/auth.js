59411f45c46fdd2b2ce3be4965a3afbafdecf9d3
Mario Haleem <mariohaleem@gmail.com>
2021-10-28 01:04:23 +0200

    add a get route to getSignup method

--- routes/auth.js
+++ routes/auth.js
@@ -6,4 +6,6 @@ const authController = require("../controllers/auth");
 
 router.get('/login', authController.getLogin);
 
+router.get('/signup', authController.getSignup);
+
 module.exports = router;
