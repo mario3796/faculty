57333d827ea8ab446ab2e3c1f308d0d4d2371ea4
Mario Haleem <mariohaleem@gmail.com>
2021-10-28 10:46:57 +0200

    add postSignup method

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,6 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
+
 exports.getLogin = (req, res, next) => {
     res.render('auth/login', {
         path: '/auth/login',
@@ -11,3 +14,23 @@ exports.getSignup = (req, res, next) => {
         title: 'Signup'
     })
 };
+
+exports.postSignup = (req, res, next) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.pwd;
+    bcrypt.hash(password, 12)
+    .then(hashedPassword => {
+        const user = new User();
+        user.name = name;
+        user.email = email;
+        user.password = hashedPassword;
+        user.user_type = 'admin';
+        return user.save();
+    })
+    .then(result => {
+        console.log(result);
+        res.redirect('/auth/login');
+    })
+    .catch(err => console.log(err));
+}
