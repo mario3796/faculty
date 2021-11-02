67182e0273105c23b5e7a15123d14c70abb8d1de
Mario Haleem <mariohaleem@gmail.com>
2021-11-02 11:10:10 +0200

    edit a middleware

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
     if(!req.session.user) {
+        res.locals.userType = null;
         return next();
     }
     User.findById(req.session.user._id)
