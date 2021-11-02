ce9191bb4bc59567277d044154f2f4e8195efa2f
Mario Haleem <mariohaleem@gmail.com>
2021-11-02 11:14:05 +0200

    add getAddCourse method

--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,7 @@ const User = require("../models/user");
 const Course = require("../models/course");
 
 exports.getIndex = (req, res, next) => {
-  User.find({ user_type: {$ne: 'admin'} })
+  User.find({ user_type: { $ne: "admin" } })
     .then((users) => {
       res.render("index", {
         path: "/",
@@ -68,69 +68,103 @@ exports.getCourses = async (req, res, next) => {
 exports.getCourseDetails = (req, res, next) => {
   const courseId = req.params.courseId;
   Course.findById(courseId)
-  .then(course => {
-    res.render('course-details', {
-      path: '/course-details',
-      title: 'Course Details',
-      course: course
+    .then((course) => {
+      res.render("course-details", {
+        path: "/course-details",
+        title: "Course Details",
+        course: course,
+      });
     })
-  })
-  .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 
 exports.getInstructorCourses = (req, res, next) => {
-    Course.find({instructorId: req.user._id})
-    .then(courses => {
-        res.render('courses', {
-            path: '/instructor-courses',
-            title: 'Instructor Courses',
-            courses: courses
-        });
+  Course.find({ instructorId: req.user._id })
+    .then((courses) => {
+      res.render("courses", {
+        path: "/instructor-courses",
+        title: "Instructor Courses",
+        courses: courses,
+      });
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
+};
+
+exports.getAddCourse = (req, res, next) => {
+  Course.find()
+    .then((courses) => {
+      let remainingCourses = [];
+      if (req.user.courses.length > 0) {
+        console.log(req.user.courses);
+        courses.forEach((i) => {
+          let isRegistered = false;
+          req.user.courses.forEach((j) => {
+            if (i._id.toString() == j._id.toString()) {
+              isRegistered = true;
+            }
+          });
+          if (!isRegistered) {
+            remainingCourses.push(i);
+          }
+        });
+        if(remainingCourses.length > 0) {
+          courses = [...remainingCourses];
+        } else {
+          courses = [];
+        }
+      }
+      User.find({ user_type: "instructor" }).then((users) => {
+        res.render("courses", {
+          path: "/add-course",
+          title: "Register Course",
+          courses: courses,
+          users: users,
+        });
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.postAddCourse = (req, res, next) => {
   const courseId = req.body.courseId;
   User.findById(req.user._id)
-  .then(user => {
-    Course.findById(courseId)
-    .then(course => {
-      user.courses.push(course);
-      return user.save();
+    .then((user) => {
+      Course.findById(courseId).then((course) => {
+        user.courses.push(course);
+        return user.save();
+      });
     })
-  })
-  .then(result => {
-    res.redirect('/');
-  })
-  .catch(err => console.log(err));
-}
+    .then((result) => {
+      res.redirect("/add-course");
+    })
+    .catch((err) => console.log(err));
+};
 
 exports.getStudentCourses = async (req, res, next) => {
   User.findById(req.user._id)
-  .then(user => {
-    const courses = user.courses;
-    User.find({user_type: 'instructor'})
-    .then(users => {
-      return res.render('courses', {
-        path: '/student-courses',
-        title: 'Student Courses',
-        courses: courses,
-        users: users
-      })
+    .then((user) => {
+      const courses = user.courses;
+      User.find({ user_type: "instructor" }).then((users) => {
+        return res.render("courses", {
+          path: "/student-courses",
+          title: "Student Courses",
+          courses: courses,
+          users: users,
+        });
+      });
     })
-  })
-  .catch(err => console.log(err));
-}
+    .catch((err) => console.log(err));
+};
 
-exports.postDeleteCourse =  (req, res, next) => {
+exports.postDeleteCourse = (req, res, next) => {
   const courseId = req.body.courseId;
   let courses = [...req.user.courses];
-  courses = courses.filter(course => course._id.toString() != courseId);
+  courses = courses.filter((course) => course._id.toString() != courseId);
   req.user.courses = [...courses];
-  req.user.save()
-  .then(result => {
-    res.redirect("/student-courses");
-  })
-  .catch(err => console.log(err));
+  req.user
+    .save()
+    .then((result) => {
+      res.redirect("/student-courses");
+    })
+    .catch((err) => console.log(err));
 };
