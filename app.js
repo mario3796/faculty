271bec65c0f0806155721a1a38fdcf04684b38bd
Mario Haleem <mariohaleem@gmail.com>
2021-11-07 18:29:01 +0200

    add error handling middleware

--- app.js
+++ app.js
@@ -65,6 +65,14 @@ app.use(userRoutes);
 app.use('/auth', authRoutes);
 app.use('/admin', adminRoutes);
 app.use(errorController.get404);
+app.get('/500', errorController.get500);
+
+app.use((error, req, res, next) => {
+    res.status(500).render('500', {
+        path: '/500',
+        title: 'Error!'
+    });
+});
 
 mongoose
 .connect(MONGODB_URL)
