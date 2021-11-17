b7be13eb5845413ee268f085ddb36af515648346
Mario Haleem <mariohaleem@gmail.com>
2021-11-17 23:04:34 +0200

    edit post route to postEditProfile method

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const { body } = require("express-validator");
 
 const router = express.Router();
 
 const userController = require("../controllers/user");
+const User = require("../models/user");
 
 router.get('/', userController.getIndex);
 
@@ -30,7 +32,20 @@ router.get('/profile/:userId', userController.getProfile);
 
 router.get('/edit-profile/:userId', userController.getEditProfile);
 
-router.post('/edit-profile', userController.postEditProfile);
+router.post('/edit-profile', [
+    body('fname').trim().isLength({min: 3}).withMessage('please enter a valid name!'),
+    body('lname').trim().isLength({min: 3}).withMessage('please enter a valid name!'),
+    body('email').notEmpty().isEmail().withMessage('please enter a valid email!')
+    .custom((value, {req}) => {
+        return User.findOne({email: value})
+        .then(user => {
+            if (user && user._id != req.body.userId) {
+                return Promise.reject("email already exists!");
+            }
+        })
+    }),
+    body('pwd').trim().isLength({min: 5}).withMessage('password must not less 5 characters!'),
+], userController.postEditProfile);
 
 router.get('/course-students/:courseId', userController.getCourseStudents);
 
